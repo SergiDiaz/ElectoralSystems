230407259b3a9a385d79c1703e2917243f0a5b44
Sergi Díaz <sdiaz@blitsoftware.com>
2017-06-27 23:40:38 +0200

    First push

---
+++ VUT/SimulateVistalegre2.js
@@ -0,0 +1,73 @@
+var indexVoter;
+var outputProgress;
+var resultsTable;
+
+
+function start() {
+	outputProgress = document.getElementById("progress");
+	resultsTable = document.getElementById("results");
+	var resultsVistalegre2 = jsonVistalegre2["payload"]["results"]["questions"][4];
+	simulate(resultsVistalegre2);
+};
+
+function simulate(resultsByCandidate) {
+
+	results.totalVotes = resultsByCandidate["totals"]["valid_votes"] - resultsByCandidate["totals"]["blank_votes"];
+
+	results.voters = [];
+
+	answersByCandidate = resultsByCandidate["answers"];
+
+	for(var i = 0; i < answersByCandidate.length; ++i) {
+		outputProgress.textContent = "Creating candidate " + i;
+		candidates[i] = {};
+		candidates[i].votes = 0;
+		candidates[i].category = answersByCandidate[i].category;
+		candidates[i].id = answersByCandidate[i].id;
+		candidates[i].text = answersByCandidate[i].text;
+	}
+
+	indexVoter = 0;
+	outputProgress.textContent = "Simulating voter " + indexVoter;
+	setTimeout(simulateVoter, 1);
+};
+
+//Simulates a voter by using a 1st vote, then a 2nd vote, then a 3rd vote, etc.
+function simulateVoter() {
+	if(indexVoter < results.totalVotes) {
+		for(var j = 0; j < answersByCandidate.length; ++j) {
+			answersByCandidate[j].used = false;
+		}
+		results.voters[indexVoter] = {};
+		results.voters[indexVoter].votes = [];
+		for(var position = 0; position < 62; ++position) {
+			//Looks for a vote in position 'position' and takes it out from the results.
+			for(var j = 0; j < answersByCandidate.length; ++j) {
+				if(answersByCandidate[j].used) {
+					//A voter can't vote for the same candidate more than once.
+					continue;
+				}
+				if(answersByCandidate[j].voters_by_position[position] > 0) {
+					answersByCandidate[j].used = true;
+					answersByCandidate[j].voters_by_position[position]--;
+					results.voters[indexVoter].votes[position] = {};
+					results.voters[indexVoter].votes[position].category = answersByCandidate[j].category;
+					results.voters[indexVoter].votes[position].id = answersByCandidate[j].id;
+					results.voters[indexVoter].votes[position].text = answersByCandidate[j].text;
+					results.voters[indexVoter].votes[position].indexCandidate = j;
+					break;
+				}
+			}
+			if(j == answersByCandidate.length) {
+				break;
+			}
+		}
+		indexVoter++;
+		outputProgress.textContent = "Simulating voter " + indexVoter;
+		setTimeout(simulateVoter, 1);
+	}
+	else {
+		outputProgress.textContent = "Preparing VUT...";
+		setTimeout(prepareVUT, 1);
+	}
+};
---
+++ VUT/VUT.js
@@ -0,0 +1,157 @@
+var candidates = [];
+var results = {};
+var answersByCandidate;
+var droopQuota;
+var indexCandidate;
+var numCandidatesElected;
+var numCandidatesEliminated;
+var firstDistributionDone = false;
+var candidateElectedLastPass;
+
+function onLoad() {
+
+};
+
+function prepareVUT() {
+	for(var i = 0; i < results.voters.length; ++i) {
+		results.voters[i].curIndexVote = 0;
+		results.voters[i].curVoteValue = 1;
+	}
+	droopQuota = Math.floor(results.totalVotes / 63 + 1);
+	numCandidatesElected = 0;
+	numCandidatesEliminated = 0;
+	indexCandidate = 0;
+	candidateElectedLastPass = false;
+	outputProgress.textContent = "Calculating VUT...";
+	setTimeout(calculate, 1);
+};
+
+function calculate() {
+	if(!firstDistributionDone)
+	{
+		for(var i = 0; i < results.voters.length; ++i) {
+			var curIndexVote = results.voters[i].curIndexVote;
+			if(results.voters[i].votes[curIndexVote].id == candidates[indexCandidate].id) {
+				candidates[indexCandidate].votes++;
+			}
+		}
+	}
+	var candidate = candidates[indexCandidate];
+	if(!candidate.elected && candidate.votes >= droopQuota) {
+		electCandidate(candidate);
+	}
+	indexCandidate++;
+	if(indexCandidate < candidates.length) {
+		setTimeout(calculate, 1);
+	}
+	else if(numCandidatesElected < 62) {
+		firstDistributionDone = true;
+		if(candidateElectedLastPass) {
+			setTimeout(transferSurplus, 1);
+		}
+		else {
+			setTimeout(eliminateCandidate, 1);
+		}
+	}
+	else {
+		outputProgress.textContent = "Done!";
+	}
+};
+
+function electCandidate(candidate) {
+	numCandidatesElected++;
+	var row = document.createElement("tr");
+	var column = document.createElement("td");
+	var txt = document.createElement("p");
+	txt.textContent = "" + numCandidatesElected + " - " + candidate.text;
+	column.appendChild(txt);
+	row.appendChild(column);
+	resultsTable.appendChild(row);
+	candidate.elected = true;
+	candidateElectedLastPass = true;
+}
+
+function transferSurplus() {
+	var surplusTransfered = false;
+	for(var i = 0; i < candidates.length; ++i) {
+		if(candidates[i].votes > droopQuota) {
+			surplusTransfered = true;
+			var surplus = candidates[i].votes - droopQuota;
+			var voteValue = surplus / candidates[i].votes;
+			candidates[i].votes = droopQuota;
+			transferVotesFromCandidate(candidates[i], voteValue);
+		}
+	}
+
+	setTimeout(eliminateCandidate, 1);
+};
+
+function transferVotesFromCandidate(candidate, voteValue) {
+	for(var i = 0; i < results.voters.length; ++i) {
+		var voter = results.voters[i];
+		var curIndexVote = voter.curIndexVote;
+		if(curIndexVote < voter.votes.length) {
+			if(voter.votes[curIndexVote].id == candidate.id) {
+				curIndexVote++;
+				if(curIndexVote < voter.votes.length) {
+					var newCandidate = candidates[voter.votes[curIndexVote].indexCandidate];
+					while(curIndexVote < voter.votes.length && (newCandidate.elected || newCandidate.eliminated)) {
+						curIndexVote++;
+						if(curIndexVote < voter.votes.length) {
+							if(voter.votes[curIndexVote]) {
+								newCandidate = candidates[voter.votes[curIndexVote].indexCandidate];
+							}
+						}
+					}
+					voter.curIndexVote = curIndexVote;
+					if(curIndexVote < voter.votes.length) {
+						var curVoteValue = voteValue * voter.curVoteValue;
+						voter.curVoteValue = curVoteValue;
+						candidates[voter.votes[curIndexVote].indexCandidate].votes += curVoteValue;
+					}
+				}
+			}
+		}
+	}
+};
+
+function eliminateCandidate() {
+	var candidateToEliminate;
+	var found = false;
+	var numVotes = droopQuota*2;
+	for(var i = 0; i < candidates.length; ++i) {
+		var candidate = candidates[i];
+		if(!candidate.elected && !candidate.eliminated) {
+			if(candidate.votes < numVotes) {
+				numVotes = candidate.votes;
+				candidateToEliminate = candidate;
+				found = true;
+			}
+		}
+	}
+
+	if(found) {
+		candidateToEliminate.eliminated = true;
+		candidateToEliminate.votes = 0;
+		numCandidatesEliminated++;
+		if(numCandidatesEliminated >= candidates.length - 62) {
+			electNonEliminatedCandidates();
+		}
+		else {
+			transferVotesFromCandidate(candidateToEliminate, 1);
+		}
+	}
+	indexCandidate = 0;
+	setTimeout(calculate, 1);
+}
+
+function electNonEliminatedCandidates() {
+	for(var i = 0; i < candidates.length; ++i) {
+		var candidate = candidates[i];
+		if(!candidate.elected && !candidate.eliminated) {
+			electCandidate(candidate);
+		}
+	}
+
+	outputProgress.textContent = "Done!";
+}
